9bcbd02bbfa33b53730698ce3987a3d1cb955fa0
Willlumm <william.lumme@gmail.com>
2024-05-21 21:55:28 +0100

    Set up bare-bones bot

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tourney Bot
+
+## How to create discord bot
+1. https://discord.com/developers/applications
+2. New Application and enter bot name
+3. Bot -> Privileged Gateway Intents -> enable Message Content Intent
+4. Bot -> Reset Token -> copy token and paste in config.py
+5. OAuth2 -> OAuth2 URL Generator -> select bot
+6. Under Bot Permissions, select the desired permissions
+7. Copy the generated url and paste into browser
+8. Add bot to server
+9. Run main.py to start bot
---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+import config
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    
+    if not message.content.startswith("!"):
+        return
+    
+    if message.content.startswith("!hello"):
+        await message.channel.send(f"Hello {message.author.mention}!")
+
+client.run(config.token)
+
+
