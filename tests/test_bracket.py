daec6fac5562962348e00b5ce4d275f78220199a
Willlumm <william.lumme@gmail.com>
2024-06-08 01:56:28 +0100

    init_bracket

--- tests/test_bracket.py
+++ tests/test_bracket.py
@@ -2,73 +2,111 @@ import pytest
 
 from tourney_bot.bracket import Match, Bracket
 
-# Match tests
+def test_match_set_score():
+    match = Match(("a", "b"))
+    match.set_score("a", 0)
+    assert match.scores == [0, None]
 
-ids = [
-    "0/0 teams",
-    "0/1 teams",
-    "1/1 teams",
-    "0/2 teams",
-    "1/2 teams",
-    "_:_",
-    "0:_",
-    "0:0",
-    "1:0"
-]
-
-match_data = [
-    ([], []),
-    ([None], []),
-    (["a"], []),
-    ([None, None], []),
-    (["a", None], []),
-    (["a", "b"], []),
-    (["a", "b"], [0, None]),
-    (["a", "b"], [0, 0]),
-    (["a", "b"], [1, 0])
-]
-
-@pytest.fixture()
-def 
-
-@pytest.fixture(params=match_data, ids=ids)
-def match_test_cases(request):
-    teams, scores = request.param
-    match = Match(*teams)
+@pytest.mark.parametrize(
+    argnames = ["teams", "scores", "expected"],
+    argvalues = [
+        pytest.param([None], [], False, id="0/1 teams"),
+        pytest.param(["a"], [], True, id="1/1 teams"),
+        pytest.param([None, None], [], False, id="0/2 teams"),
+        pytest.param(["a", None], [], False, id="1/2 teams"),
+        pytest.param(["a", "b"], [], False, id="2/2 teams, -:- score"),
+        pytest.param(["a", "b"], [0], False, id="2/2 teams, 0:- score"),
+        pytest.param(["a", "b"], [0, 0], True, id="2/2 teams, 0:0 score"),
+        pytest.param(["a", "b"], [1, 0], True, id="2/2 teams, 1:0 score")
+    ]
+)
+def test_match_is_finished(teams, scores, expected):
+    match = Match(teams)
     for team, score in zip(teams, scores):
-        match.scores[team] = score
-    return match
-
-@pytest.mark.parametrize(("id", "match", "expected"), setup_match_test_cases([True, False, True, False, False, False, False, True, True]))
-
-@pytest.fixture(params=)
-def test_match_is_finished(match_test_cases, expected):
+        match.set_score(team, score)
     assert match.is_finished() == expected
 
-@pytest.mark.parametrize("expected", [None, None, "a", None, None, None, None, None, "a"])
-def test_match_get_winner(expected):
-    match = Match(*teams)
+@pytest.mark.parametrize(
+    argnames = ["teams", "scores", "expected"],
+    argvalues = [
+        pytest.param(["a"], [], "a", id="Bye"),
+        pytest.param(["a", "b"], [], None, id="Not finished"),
+        pytest.param(["a", "b"], [0, 0], None, id="Draw"),
+        pytest.param(["a", "b"], [1, 0], "a", id="1:0"),
+    ]
+)
+def test_match_get_winner(teams, scores, expected):
+    match = Match(teams)
     for team, score in zip(teams, scores):
-        match.scores[team] = score
+        match.set_score(team, score)
     assert match.get_winner() == expected
 
-def test_match_get_winner_not_finished():
-    match = Match()
-    assert match.get_winner() is None
-
-def test_match_get_winner_draw():
-    match = Match("a", "b")
-    match.scores["a"] = 0
-    match.scores["b"] = 0
-    assert match.get_winner() is None
-
-# Bracket tests
-
-def test_bracket_get_num_teams():
-    teams = ["1", "2", "3", "4"]
-    final_match = Match()
-    assert Bracket(teams).matches == [
-        Match("1", "4", next_match=final_match),
-        Match("2", "3", next_match=final_match),
-        final_match
+@pytest.mark.parametrize(
+    argnames = ["num_teams", "expected"],
+    argvalues= [
+        (0, 0),
+        (1, 0),
+        (2, 1),
+        (3, 2),
+        (4, 2),
+        (5, 3),
+        (8, 3),
+        (9, 4),
+        (16, 4),
+        (17, 5),
+        (32, 5),
+        (33, 6),
+        (64, 6),
+        (65, 7)
     ]
+)
+def test_bracket_get_num_rounds(num_teams, expected):
+    assert Bracket.get_num_rounds(num_teams) == expected
+
+@pytest.mark.parametrize(
+    argnames = ["num_teams", "expected"],
+    argvalues= [
+        (2, [1, 2]),
+        (3, [1, None, 2, 3]),
+        (4, [1, 4, 2, 3]),
+        (5, [1, None, 4, 5, 2, None, 3, None]),
+        (8, [1, 8, 4, 5, 2, 7, 3, 6]),
+        (9, [1, None, 8, 9, 4, None, 5, None, 2, None, 7, None, 3, None, 6, None]),
+        (16, [1, 16, 8, 9, 4, 13, 5, 12, 2, 15, 7, 10, 3, 14, 6, 11])
+    ]
+)
+def test_bracket_arrange_seeds(num_teams, expected):
+    assert Bracket.arrange_seeds(num_teams) == expected
+
+@pytest.mark.parametrize(
+    argnames = ["teams", "expected"],
+    argvalues= [
+        (["a"], [("a", None)]),
+        (["a", "b"], [("a", "b")]),
+        (["a", "b", "c"], [("a", None), ("b", "c")]),
+        (["a", "b", "c", "d"], [("a", "d"), ("b", "c")]),
+        (["a", "b", "c", "d", "e"], [("a", None), ("d", "e"), ("b", None), ("c", None)]),
+        (["a", "b", "c", "d", "e", "f", "g", "h"], [("a", "h"), ("d", "e"), ("b", "g"), ("c", "f")])
+    ]
+)
+def test_bracket_arrange_teams(teams, expected):
+    assert Bracket.arrange_teams(teams) == expected
+
+@pytest.mark.parametrize(
+    argnames = ["teams", "expected_num_matches"],
+    argvalues= [
+        (["a"], 0),
+        (["a", "b"], 1),
+        (["a", "b", "c"], 3),
+        (["a", "b", "c", "d"], 3),
+        (["a", "b", "c", "d", "e"], 7),
+        (["a", "b", "c", "d", "e", "f", "g", "h"], 7),
+        (["a", "b", "c", "d", "e", "f", "g", "h", "i"], 15),
+        (["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"], 15),
+        (["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"], 31)
+    ]
+)
+def test_bracket_init_matches(teams, expected_num_matches):
+    bracket = Bracket(teams)
+    assert len(bracket.matches) == expected_num_matches
+    assert all(isinstance(match, Match) for match in bracket.matches)
--- tourney_bot/bracket.py
+++ tourney_bot/bracket.py
@@ -1,43 +1,110 @@
-from math import log2
-from typing import Self
+from math import ceil, log2
+from typing import Self, Sequence
 
 class Match:
     
-    def __init__(self, *teams: str, next_match: Self=None) -> None:
-        self.scores = {team: None for team in teams}
+    def __init__(self, teams: Sequence[str], next_match: Self = None) -> None:
+        self.teams = list(teams)
+        self.scores = [None] * len(teams)
         self.next_match = next_match
 
+    def to_str(self, width: int = None) -> str:
+        width = width if width else 10
+        score_width = 3
+        team_width = width - score_width
+        team_strs = []
+        for team, score in zip(self.teams, self.scores):
+            team = team[:team_width].ljust(team_width) if team else ""
+            score = score if score else "-"
+            team_strs.append(f"{team:<10} {score:>}")
+        return "\n".join(team_strs)
+
+    def set_score(self, team: str, score: int) -> None:
+        i = self.teams.index(team)
+        self.scores[i] = score
+
     def is_finished(self) -> bool:
-        if not self.scores:
+        if None in self.teams:
             return False
-        elif len(self.scores) == 1:
+        elif len(self.teams) == 1:
             return True
-        return all(score is not None for score in self.scores.values())
+        elif None in self.scores:
+            return False
+        return True
 
     def get_winner(self) -> str:
         if not self.is_finished():
             return
-        max_score =  max(self.scores.values())
-        winners = [team for team, score in self.scores.items() if score == max_score]
-        if len(winners) > 1:
-            return
-        return winners[0]
+        max_score = max(self.scores)
+        winners = [team for team, score in zip(self.teams, self.scores) if score == max_score]
+        if len(winners) == 1:
+            return winners[0]
 
 class Bracket:
 
     def __init__(self, teams: list[str]) -> None:
-        for round in range(len(teams)):
-            pass
-    
+        self.matches = Bracket.init_matches(teams)
+
     def __str__(self) -> str:
-        pass
+        matches = []
+        for i, match in enumerate(self.matches):
+            matches.append(f"{match.to_str()}")
+        return "\n\n".join(matches)
 
     @staticmethod
     def get_num_rounds(num_teams: int) -> int:
-        pass
+        if num_teams < 2:
+            return 0
+        return int(ceil(log2(num_teams)))
+
+    @staticmethod
+    def arrange_seeds(num_teams: int) -> list[int]:
+        seeds = [1, 2]
+        for round in range(2, Bracket.get_num_rounds(num_teams) + 1):
+            new_seeds = []
+            for seed in seeds:
+                new_seeds += [seed, 2**round - seed + 1]
+            seeds = new_seeds
+        return [seed if seed <= num_teams else None for seed in seeds]
+
+    @staticmethod
+    def arrange_teams(teams: list[str]) -> list[tuple[str]]:
+        seeds = Bracket.arrange_seeds(len(teams))
+        arranged_teams = []
+        for i in range(0, len(seeds), 2):
+            seed1, seed2 = seeds[i : i+2]
+            team1 = teams[seed1 - 1] if seed1 else None
+            team2 = teams[seed2 - 1] if seed2 else None
+            arranged_teams.append((team1, team2))
+        return arranged_teams
+    
+    @staticmethod
+    def init_matches(teams: list[str]) -> list[Match]:
+        rounds = []
+        num_rounds = Bracket.get_num_rounds(len(teams))
+        for round_i in range(num_rounds):
+            round = []
+            team_pairs = [(None, None)] * (2 ** round_i)
+            if round_i == num_rounds - 1:
+                team_pairs = Bracket.arrange_teams(teams)
+            for match_i, team_pair in enumerate(team_pairs):
+                match = Match(team_pair)
+                if round_i != 0:
+                    match.next_match = rounds[round_i - 1][match_i // 2]
+                round.append(match)
+            rounds.append(round)
+        matches = []
+        for round in reversed(rounds):
+            matches.extend(round)
+        return matches
+
 
     def get_next_match(self) -> Match:
         pass
 
     def submit_score(self, match: Match) -> None:
         pass
+
+bracket = Bracket(["really long team name", "b", "c", "d", "e", "f", "g", "h"])
+print(bracket)
+print(bracket.matches)
